c40fefa0ec3058105dd15a4e37d58bf7132f05ab
BRAINIAC2677 <brainiac2677@gmail.com>
2023-07-21 00:46:08 +0600

    some restructuring

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
 # CSE-208-Data-Structures-And-Algorithms-2
 
-## Contents:
-- MST|Minimum Spanning Tree
+Here is the [`Course Outline.`](/assets/course_outline.pdf)
+
+## Offline Assignments:
+- Week-3 | Minimum Spanning Tree
   - [Kruskal's Algorithm](/w-3/offline/kruskals.cpp)
   - [Prim's Algorithm](/w-3/offline/prims.cpp)
-- SSSP|Single Source Shortest Path
+- Week-4 | Single Source Shortest Path
   - [Djikstra Algorithm](/w-4/offline/djikstra.cpp)
   - [Bellman-Ford Algorithm](/w-4/offline/bellman_ford.cpp)
-- APSP|All Pair Shortest Path
+- Week-5 | All Pair Shortest Path
   - [DP by Matrix Multiplication](/w-5/offline/matrix_multiplication.cpp)
   - [Floyd-Warshall Algorithm](/w-5/offline/floyd_warshall.cpp)
-- Max Flow
+- Week-6 | Max Flow
   - [Baseball Elimination Problem](/w-6/offline/baseball_elimination.cpp)
-- Heap
+- Week-8 | Heap
   - [Fibonacci Heap](/w-8/offline/fibonacci_heap.h)
   - [Binary Heap](/w-8/offline/binary_heap.h)
+- Week-9 | Red Black Tree
+  - [Red Black Tree](/w-9/offline/red_black_tree.h)
+- Week-10 | Hash Table
+  - [Hash Table](/w-10/offline/hash.h)
+- Week-13 | Band Matrix Algorithm
+  - [Band Matrix Algorithm](/w-13/offline/1905004.cpp)
 
+## Online Assignments:
+- [Week-2 | Graph Algorithms](/w-2/online)
+- [Week-3 | Minimum Spanning Tree](/w-3/online)
+- [Week-4 | Single Source Shortest Path](/w-4/online)
+- [Week-5 | All Pair Shortest Path](/w-5/online)
+- [Week-6 | Max Flow](/w-6/online)
+- [Week-10 | Hash Table](/w-10/online)
 
 ## Relevant QnA:
  - [declaring a priority_queue in c++ with a custom comparator](https://stackoverflow.com/a/16111402/13148347)
+ - [llvm supported standard library](https://clang.llvm.org/docs/Toolchain.html#c-standard-library)
+ - [c and c++ standard library](https://www.internalpointers.com/post/c-c-standard-library)
--- w-13/offline/1905004.cpp
+++ w-13/offline/1905004.cpp
@@ -5,93 +5,19 @@ NO DREAM IS TOO BIG AND NO DREAMER IS TOO SMALL*/
 #include <bits/stdc++.h>
 using namespace std;
 
-//=========================================== debugger ================================================
-void __print(int x) { cerr << x; }
-void __print(long x) { cerr << x; }
-void __print(long long x) { cerr << x; }
-void __print(unsigned x) { cerr << x; }
-void __print(unsigned long x) { cerr << x; }
-void __print(unsigned long long x) { cerr << x; }
-void __print(float x) { cerr << x; }
-void __print(double x) { cerr << x; }
-void __print(long double x) { cerr << x; }
-void __print(char x) { cerr << '\'' << x << '\''; }
-void __print(const char *x) { cerr << '\"' << x << '\"'; }
-void __print(const string &x) { cerr << '\"' << x << '\"'; }
-void __print(bool x) { cerr << (x ? "true" : "false"); }
-
-template <typename T, typename V>
-void __print(const pair<T, V> &x)
-{
-    cerr << '{';
-    __print(x.first);
-    cerr << ',';
-    __print(x.second);
-    cerr << '}';
-}
-template <typename T>
-void __print(const T &x)
-{
-    int f = 0;
-    cerr << '{';
-    for (auto &i : x)
-        cerr << (f++ ? "," : ""), __print(i);
-    cerr << "}";
-}
-void _print() { cerr << "]\n"; }
-template <typename T, typename... V>
-void _print(T t, V... v)
-{
-    __print(t);
-    if (sizeof...(v))
-        cerr << ", ";
-    _print(v...);
-}
-#ifndef ONLINE_JUDGE
-#define debug(x...)               \
-    cerr << "[" << #x << "] = ["; \
-    _print(x)
-#else
-#define debug(x...)
-#endif
-//=========================================== end of debugger ================================================
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<long long> vll;
-typedef vector<int> vi;
-
-#define io ios_base::sync_with_stdio(false)
-#define pb push_back
-#define is_set(n, i) bool(n & (1LL << i))
-#define bit_set(n, i) (n | (1LL << i))
-#define bit_unset(n, i) (n & !(1LL << i))
-#define PI 2 * acos(0.0)
-#define all(r) (r).begin(), (r).end()
-#define rev(r) (r).rbegin(), (r).rend()
-#define dbg(a) cout << #a << " ->->->-> " << a << "\n"
-
-int dirx[] = {1, -1, 0, 0, 1, 1, -1, -1}, diry[] = {0, 0, 1, -1, 1, -1, 1, -1};
-const int maxn = 100009;
-const int maxv = 1000000009;
-const int mod = 1000000007;
-const long long int inf = 1000000000000000000;
-//=============================================ASIFAZAD==============================================//
-
-#define ZERO 'o'
-#define NONZERO 'x'
+#define ZERO '0'
+#define NONZERO 'X'
 #define NONE '.'
 
 class BandMatrix
 {
     int n, fixed_col_n, fixed_row_n, branch_depth, branch_pos;
-    char **mat;
+    vector<vector<char>> mat;
 
 public:
     BandMatrix(int n, int fixed_col_n, int fixed_row_n, int branch_depth, int branch_pos);
     void inmat();
+    void set_mat(vector<vector<char>> mat);
     void printmat();
     int get_lower_bound();
     int get_n();
@@ -99,7 +25,32 @@ public:
     int get_fixed_row_n();
     int get_branch_depth();
     int get_branch_pos();
-    ~BandMatrix();
+    vector<vector<char>> get_mat();
+};
+
+class Compare
+{
+public:
+    bool operator()(BandMatrix a, BandMatrix b)
+    {
+        if (a.get_lower_bound() > b.get_lower_bound())
+        {
+            return true;
+        }
+        else if (a.get_lower_bound() < b.get_lower_bound())
+        {
+            return false;
+        }
+        else if (a.get_branch_depth() < b.get_branch_depth())
+        {
+            return true;
+        }
+        else if (a.get_branch_depth() > b.get_branch_depth())
+        {
+            return false;
+        }
+        return a.get_branch_pos() < b.get_branch_pos();
+    }
 };
 
 BandMatrix::BandMatrix(int n, int fixed_col_n, int fixed_row_n, int branch_depth, int branch_pos)
@@ -109,13 +60,7 @@ BandMatrix::BandMatrix(int n, int fixed_col_n, int fixed_row_n, int branch_depth
     this->fixed_row_n = fixed_row_n;
     this->branch_depth = branch_depth;
     this->branch_pos = branch_pos;
-    mat = new char *[n];
-    for (int i = 0; i < n; i++)
-    {
-        mat[i] = new char[n];
-        for (int j = 0; j < n; j++)
-            mat[i][j] = NONE;
-    }
+    mat.assign(n, vector<char>(n, NONE));
 }
 
 void BandMatrix::inmat()
@@ -127,6 +72,12 @@ void BandMatrix::inmat()
             cin >> mat[i][j];
         }
     }
+}
+
+void BandMatrix::set_mat(vector<vector<char>> mat)
+{
+    this->mat = mat;
+    this->n = mat.size();
 }
 
 void BandMatrix::printmat()
@@ -270,33 +221,117 @@ int BandMatrix::get_branch_pos()
     return branch_pos;
 }
 
-BandMatrix::~BandMatrix()
+vector<vector<char>> BandMatrix::get_mat()
 {
+    return mat;
+}
+
+vector<vector<char>> move_col(vector<vector<char>> gmat, int from, int to)
+{
+    int n = gmat.size();
+    vector<vector<char>> rmat(gmat);
     for (int i = 0; i < n; i++)
     {
-        delete[] mat[i];
+        rmat[i][to] = gmat[i][from];
     }
-    delete[] mat;
+    for (int j = to; j < from; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            rmat[i][j + 1] = gmat[i][j];
+        }
+    }
+    return rmat;
+}
+
+vector<vector<char>> move_row(vector<vector<char>> gmat, int from, int to)
+{
+    int n = gmat.size();
+    vector<vector<char>> rmat(gmat);
+    for (int j = 0; j < n; j++)
+    {
+        rmat[to][j] = gmat[from][j];
+    }
+    for (int i = to; i < from; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            rmat[i + 1][j] = gmat[i][j];
+        }
+    }
+    return rmat;
 }
 
 void solve(int t)
 {
-    int n, fixed_col_n, fixed_row_n;
-    cin >> n >> fixed_col_n >> fixed_row_n;
-    BandMatrix bm(n, fixed_col_n, fixed_row_n, 0, 0);
+    int n;
+    cin >> n;
+    BandMatrix bm(n, 0, 0, 0, 0);
     bm.inmat();
-    cout << "\n";
-    bm.printmat();
-    cout << bm.get_lower_bound() << "\n";
+    int bandwidth = n;
+    vector<vector<char>> reduced_mat = bm.get_mat();
+    priority_queue<BandMatrix, vector<BandMatrix>, Compare> pq;
+    pq.push(bm);
+    while (!pq.empty())
+    {
+        BandMatrix cm = pq.top();
+        pq.pop();
+        int lower_bound = cm.get_lower_bound();
+        if (bandwidth <= lower_bound)
+        {
+            break;
+        }
+        int fixed_col_n = cm.get_fixed_col_n();
+        int fixed_row_n = cm.get_fixed_row_n();
+        int branch_depth = cm.get_branch_depth();
+        // n-1 cols and rows are fixed
+        if (fixed_col_n == fixed_col_n && fixed_col_n >= n - 1)
+        {
+            if (bandwidth > lower_bound)
+            {
+                bandwidth = lower_bound;
+                reduced_mat = cm.get_mat();
+            }
+        }
+        // fixing another col
+        else if (fixed_col_n == fixed_row_n)
+        {
+            for (int j = fixed_col_n; j < n; j++)
+            {
+                BandMatrix nm = BandMatrix(n, fixed_col_n + 1, fixed_row_n, branch_depth + 1, j - fixed_col_n);
+                nm.set_mat(move_col(cm.get_mat(), j, fixed_col_n));
+                pq.push(nm);
+            }
+        }
+        // fixing another row
+        else
+        {
+            for (int i = fixed_row_n; i < n; i++)
+            {
+                BandMatrix nm = BandMatrix(n, fixed_col_n, fixed_row_n + 1, branch_depth + 1, i - fixed_row_n);
+                nm.set_mat(move_row(cm.get_mat(), i, fixed_row_n));
+                pq.push(nm);
+            }
+        }
+    }
+    cout << "\nbandwidth: " << bandwidth << "\n";
+    cout << "reduced matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << reduced_mat[i][j];
+        }
+        cout << "\n";
+    }
 }
 
 int main()
 {
-    io;
     ifstream in("input.txt");
     ofstream out("output.txt");
-    int t;
-    cin >> t;
+    int t = 1;
+    // cin >> t;
     for (int i = 1; i <= t; i++)
     {
         solve(i);
